796b94c683769588a3e3522519cef39fb9116b82
Daimonke <Daimonas98@gmail.com>
2022-05-17 15:17:49 +0300

    asd

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ import Script from 'next/script'
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const data = await getData()
-  return { props: {data}}
+  return { props: { data } }
 }
 
 interface itemTypes {
@@ -23,10 +23,9 @@ interface itemTypes {
   image: string
 }
 
-const Home: NextPage = ( {data}: any ) => {
+const Home: NextPage = ({ data }: any) => {
   return (
     <div>
-      {/* <Script src="https://apis.google.com/js/platform.js" async defer></Script> */}
       <Head>
         <title>Create Next App</title>
         <meta name="description" content="Reviews for bars" />
@@ -35,38 +34,39 @@ const Home: NextPage = ( {data}: any ) => {
         <meta name="google-signin-client_id" content="926100731368-6ibmc74vtav4tpit6mib7hrjlkd0mqq4.apps.googleusercontent.com"></meta>
       </Head>
       <div className='header'>
-        <Container sx={{display: 'flex', justifyContent: 'space-between'}}>
-        <Link href='/'>
-          <a className='logo'>
-          <Image src={logo} alt='logo' layout='responsive'></Image>
-          </a>
-        </Link>
-        <nav className='nav'>
-        <Button variant='contained' color='primary'>Login</Button>
-        <Button variant='contained' color='info'>Sign-up</Button>
-        <div style={{display: 'flex', alignItems: 'center'}} className="g-signin2" data-onsuccess="onSignIn" data-longTitle={true}>Login</div>
-        </nav>
-        
+        <Container sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Link href='/'>
+            <a className='logo'>
+              <Image src={logo} alt='logo' layout='responsive'></Image>
+            </a>
+          </Link>
+          <nav className='nav'>
+            <Button variant='contained' color='primary'>Login</Button>
+            <Button variant='contained' color='info'>Sign-up</Button>
+            <div style={{ display: 'flex', alignItems: 'center' }} className="g-signin2" data-onsuccess="onSignIn" data-longtitle={true}>Login</div>
+          </nav>
+
         </Container>
-        
+
       </div>
-        <Container sx={{display: 'grid', gridTemplateColumns: {sm: '1fr', md: '1fr 1fr', lg: '1fr 1fr 1fr'} , gap: 3, p: 3}}>
-          {data.map((item: itemTypes) => {
-            return (
-              <Card key={item.id}>
-                <Image className='barImage' src={item.image} alt={item.name} width='100%' height='100%' layout='responsive'></Image>
-                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'space-between', padding: '15px'}}>
+      <Container sx={{ display: 'grid', gridTemplateColumns: { sm: '1fr', md: '1fr 1fr', lg: '1fr 1fr 1fr' }, gap: 3, p: 3 }}>
+        {data.map((item: itemTypes) => {
+          return (
+            <Card key={item.id}>
+              <Image className='barImage' src={item.image} alt={item.name} width='100%' height='100%' layout='responsive'></Image>
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'space-between', padding: '15px' }}>
                 <Typography variant='body1' fontWeight={500} fontSize={20}>{item.name}</Typography>
                 <Typography variant='body1' fontSize={15} color='rgb(100, 100, 100)'>{item.address}</Typography>
-                </div>
-                <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '11px'}}>
-                <Rating  name="read-only" value={item.rating} precision={0.1} readOnly />
-                <Rating sx={{color: 'rgb(0, 182, 9)'}} icon={<EuroIcon fontSize='small'/>} emptyIcon={<EuroIcon fontSize='small' />} name="read-only" value={item.price} precision={0.1} readOnly />
-                </div>
-              </Card>
-            )
-          })}
-        </Container>
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '11px' }}>
+                <Rating name="read-only" value={item.rating} precision={0.1} readOnly />
+                <Rating sx={{ color: 'rgb(0, 182, 9)' }} icon={<EuroIcon fontSize='small' />} emptyIcon={<EuroIcon fontSize='small' />} name="read-only" value={item.price} precision={0.1} readOnly />
+              </div>
+            </Card>
+          )
+        })}
+      </Container>
+      <Script src="https://apis.google.com/js/platform.js" onLoad={() => console.log('google')}></Script>
     </div>
   )
 }
